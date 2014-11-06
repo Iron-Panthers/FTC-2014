93fd26061960e8529eeb28ab8b438ced761b3cd3
icd107 <icd107@live.com>
2014-11-05 16:32:13 -0800

    move function

--- utility.h
+++ utility.h
@@ -1,7 +1,10 @@
+const float ROBOT_RATE = 200;	//measure this to find real value
+const float MOTOR_SPEED = 75;
+
 void turnRight()
 {
-	motor[left] = 75;
-	motor[right] = 75;
+	motor[left] = MOTOR_SPEED;
+	motor[right] = MOTOR_SPEED;
 	wait1Msec(100);
 }
 
@@ -10,4 +13,15 @@ void turnLeft()
 	motor[left] = -75;
 	motor[right] = -75;
 	wait1Msec(100);
+}
+
+void move(float distance)
+{
+	int waitTime = distance/ROBOT_RATE;
+	waitTime = waitTime * 1000;
+	motor[left] = MOTOR_SPEED;
+	motor[right] = MOTOR_SPEED;
+	wait1Msec(waitTime);
+	motor[left] = 0;
+	motor[right] = 0;
 }
