91cf20dbf22a34fa727ab65b306196571ae490eb
icd107 <icd107@live.com>
2014-11-06 16:23:04 -0800

    stuff

--- utility.h
+++ utility.h
@@ -15,6 +15,7 @@ void turn(float degrees)
 			motor[right] = 0;
 			wait1Msec(time);
 			motor[left] = 0;
+			motor[right] = 0;
 		}
 	else if (degrees < 0)
 	{
@@ -22,15 +23,49 @@ void turn(float degrees)
 		motor[right] = ROBOT_SPEED;
 		wait1Msec(time);
 		motor[right] = 0;
+		motor[left] = 0;
+	}
+}
+
+void turnTwoWheels(float degrees)
+{
+	float rad = degrees * PI/180;
+	float distance = rad * RADIUS;
+	float time = distance/ROBOT_SPEED;
+	time = time * 1000;
+
+	if (degrees > 0)
+		{
+			motor[left] = ROBOT_SPEED;
+			motor[right] = -ROBOT_SPEED;
+			wait1Msec(time);
+			motor[left] = 0;
+			motor[right] = 0;
+		}
+	else if (degrees < 0)
+	{
+		motor[left] = -ROBOT_SPEED;
+		motor[right] = ROBOT_SPEED;
+		wait1Msec(time);
+		motor[right] = 0;
+		motor[left] = 0;
 	}
 }
 
 void move(float distance)
 {
-	int waitTime = distance/ROBOT_RATE;
-	waitTime = waitTime * 1000;
-	motor[left] = MOTOR_SPEED;
-	motor[right] = MOTOR_SPEED;
+	float waitTime = distance/ROBOT_RATE;
+	waitTime = abs(waitTime) * 1000;
+	if (distance > 0)
+	{
+		motor[left] = MOTOR_SPEED;
+		motor[right] = MOTOR_SPEED;
+	}
+	else if (distance < 0)
+	{
+		motor[left] = -MOTOR_SPEED;
+		motor[right] = -MOTOR_SPEED;
+	}
 	wait1Msec(waitTime);
 	motor[left] = 0;
 	motor[right] = 0;
