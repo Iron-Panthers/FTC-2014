2499f240cb01987bddc22cf5a42c414ed9566d2d
Emily Tam <18tame94@burlingamehighschool.org>
2014-11-20 15:29:33 -0800

    autonomous knock down

---
+++ Auto_knockDown.c
@@ -0,0 +1,24 @@
+#pragma config(Hubs,  S4, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Sensor, S4,     ,               sensorI2CMuxController)
+#pragma config(Motor,  motorA,           ,             tmotorNXT, openLoop, encoder)
+#pragma config(Motor,  mtr_S4_C1_1,     right,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C1_2,     left,          tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S4_C2_1,     extender,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C2_2,     intakeMotor,   tmotorTetrix, openLoop, reversed)
+#pragma config(Servo,  srvo_S4_C3_1,    servo1,               tServoStandard)
+#pragma config(Servo,  srvo_S4_C3_2,    servo2,               tServoContinuousRotation)
+#pragma config(Servo,  srvo_S4_C3_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S4_C3_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S4_C3_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S4_C3_6,    servo6,               tServoNone)
+
+
+task main()
+{
+    move(1.49);
+    turn(-90);
+    move(.68);
+    turn(90);
+    move(-1.49);
+
+}
